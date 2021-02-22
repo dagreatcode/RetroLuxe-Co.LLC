c9ea190b9a1d4fbc41eeacca2728d71f47a2e48f
dagreatcode <dagreatcode@gmail.com>
2021-02-22 17:42:03 -0500

    mvc folders done

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
+// const db = require("./models"),
 
 const PORT = process.env.PORT || 3001;
 

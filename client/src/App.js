5f61f77ce5f8b4fa18f9cbe0bc32b341f4593173
dagreatcode <dagreatcode@gmail.com>
2021-02-24 11:57:49 -0500

    added home page and fixed on navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,35 @@
-
-import './App.css';
+import { useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import About from "./pages/About/About";
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./containers/pages/Home/Home";
 
 
 function App() {
-
-
+  useEffect(() => {
+    console.log("Make an API call");
+    axios
+      .get("/api/config")
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
   return (
     <div className="App">
       <Router>
-      <Navbar />
+        <Navbar />
         <div>
           <Switch>
+          <Route exact path="/RetroLuxe-Co.LLC" component={Home} />
+          <Route exact path="/" component={Home} />
           </Switch>
         </div>
       </Router>
-      <h1>Welcome To Retro Luxe.</h1>
-      <h1>Where Your Dreams Come True!</h1>
     </div>
   );
 }
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -7,14 +7,26 @@ const Navbar = () => {
 <>
 <nav className="navbar navbar-expand-lg navbar-light bg-light">
   <div className="container-fluid">
-    <a className="navbar-brand" href="#">Navbar</a>
+    <a className="navbar-brand" href="#">Retro Luxe & Co. </a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <a className="nav-link active" aria-current="page" href="#">About</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Category</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">FQA</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Blog</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Contact</a>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="#">Link</a>

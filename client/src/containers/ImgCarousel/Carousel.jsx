a748bc70d7dff35f50d057a64ad84f51a515e5c4
dagreatcode <dagreatcode@gmail.com>
2021-02-25 18:47:28 -0500

    added img carousel

---
+++ client/src/containers/ImgCarousel/Carousel.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+// import "./styles.css";
+import AliceCarousel from "react-alice-carousel";
+import "./Carousel.css"
+import "react-alice-carousel/lib/alice-carousel.css";
+import image1 from "../../img/photo/home-1.jpg";
+import image2 from "../../img/photo/home-2.jpg";
+import image4 from "../../img/photo/home-3-pastel.jpg";
+import image5 from "../../img/photo/home-5.jpg";
+
+export default function Carousel() {
+  return (
+    <div>
+      <AliceCarousel autoPlay autoPlayInterval="3000">
+        <img src={image1} className="sliderimg" alt="" />
+        <img src={image2} className="sliderimg" alt="" />
+        <img src={image4} className="sliderimg" alt="" />
+        <img src={image5} className="sliderimg" alt="" />
+      </AliceCarousel>
+    </div>
+  );
+}
--- client/src/containers/pages/Home/Home.jsx
+++ client/src/containers/pages/Home/Home.jsx
@@ -1,12 +1,13 @@
 import React, { Component } from "react"; 
 import Countdown from "../Countdown/Countdown";
 import { BrowserRouter as Router} from "react-router-dom";
-
+import Carousel from "../../ImgCarousel/Carousel";
 
 class Home extends Component {
   render() {
     return (
       <Router>
+      <Carousel/>
       <Countdown/>
           <div>
             <h1>Welcome To Retro Luxe & Co. LLC.</h1>

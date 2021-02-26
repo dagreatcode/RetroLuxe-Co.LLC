d8ad4c475d04c20e5b7a7e4c2782aacffcf45a5b
dagreatcode <dagreatcode@gmail.com>
2021-02-25 21:56:32 -0500

    fixing top img

--- client/src/containers/ImgCarousel/Carousel.jsx
+++ client/src/containers/ImgCarousel/Carousel.jsx
@@ -6,6 +6,11 @@ import image1 from "../../img/photo/home-1.jpg";
 import image2 from "../../img/photo/home-2.jpg";
 import image4 from "../../img/photo/home-3-pastel.jpg";
 import image5 from "../../img/photo/home-5.jpg";
+import image6 from "../../img/photo/home-4.jpg";
+import image7 from "../../img/photo/deal-plain.jpg";
+import image8 from "../../img/photo/serrah-galos-494279-unsplash-parallax.jpg";
+import image9 from "../../img/photo/blog.jpg";
+
 
 export default function Carousel() {
   return (
@@ -15,6 +20,10 @@ export default function Carousel() {
         <img src={image2} className="sliderimg" alt="" />
         <img src={image4} className="sliderimg" alt="" />
         <img src={image5} className="sliderimg" alt="" />
+        <img src={image6} className="sliderimg" alt="" />
+        <img src={image7} className="sliderimg" alt="" />
+        <img src={image8} className="sliderimg" alt="" />
+        <img src={image9} className="sliderimg" alt="" />
       </AliceCarousel>
     </div>
   );

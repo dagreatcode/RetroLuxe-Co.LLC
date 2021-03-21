b05bb57292615ed4576c532560774a43c5851ebe
dagreatcode <dagreatcode@gmail.com>
2021-03-21 16:11:03 -0400

    login page added

--- client/src/containers/pages/Login/Login.jsx
+++ client/src/containers/pages/Login/Login.jsx
@@ -3,35 +3,40 @@ import React, { Component } from "react";
 class Login extends Component {
   render() {
     return (
-        <>
+      <>
         <h1>Login</h1>
-      <form className="container">
-        <div className="mb-3 row">
-          <label for="exampleFormControlInput1" className="form-label">
-            Email address
-          </label>
-          <div className="col-sm-12">
-            <input
-              type="email"
-              className="form-control"
-              id="exampleFormControlInput1"
-              placeholder="name@example.com"
-            />
+        <form className="container">
+          <div className="mb-3 row">
+            <label for="exampleFormControlInput1" className="form-label">
+              Email address
+            </label>
+            <div className="col-sm-12">
+              <input
+                type="email"
+                className="form-control"
+                id="exampleFormControlInput1"
+                placeholder="name@example.com"
+              />
+            </div>
           </div>
-        </div>
-        <div className="mb-3 row">
-          <label for="inputPassword" className="col-form-label">
-            Password
-          </label>
-          <div className="col-sm-12">
-            <input
-              type="password"
-              className="form-control"
-              id="inputPassword"
-            />
+          <div className="mb-3 row">
+            <label for="inputPassword" className="col-form-label">
+              Password
+            </label>
+            <div className="col-sm-12">
+              <input
+                type="password"
+                className="form-control"
+                id="inputPassword"
+              />
+            </div>
           </div>
-        </div>
-      </form>
+          <div class="col-12">
+            <button class="btn btn-primary" type="submit">
+              Login
+            </button>
+          </div>
+        </form>
       </>
     );
   }

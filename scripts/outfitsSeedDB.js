3bdada2c298aa9e02b30f499a35526b6e16a4eb6
dagreatcode <dagreatcode@gmail.com>
2021-03-21 00:48:25 -0400

    updates to data base

--- scripts/outfitsSeedDB.js
+++ scripts/outfitsSeedDB.js
@@ -1,13 +1,13 @@
 const mongoose = require("mongoose"),
   db = require("../models");
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/retroluxe_db"
-);
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/retroluxe_db");
 const outfitsSeedDB = [
   {
-    message_text:
-      "You have power over your mind - not outside events. Realize this, and you will find strength.",
-  }
+    type: "Pants",
+    image: "/client/src/img/product/category-jeans-colour-bg-2/jpg",
+    description: "High Riders",
+    bin_location: 1,
+  },
 ];
 db.Outfits.remove({})
   .then(() => db.Outfits.collection.insertMany(outfitsSeedDB))

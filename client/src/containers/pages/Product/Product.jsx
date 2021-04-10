9ebb3bb25e9f5d94337bb888dfb0ef482d522e8b
dagreatcode <dagreatcode@gmail.com>
2021-04-10 10:18:40 -0400

    update home

--- client/src/containers/pages/Product/Product.jsx
+++ client/src/containers/pages/Product/Product.jsx
@@ -5,27 +5,27 @@ import data from "../Home/data";
 export default function Product(props) {
   const { product } = props;
   return (
-
-        <div key={product._id} className="card">
+    <div className="col-3">
+      <div key={product._id} className="card">
+        <a href={`/product/${product._id}`}>
+          <h2>{product.name}</h2>
+        </a>
+        <div className="card-body">
           <a href={`/product/${product._id}`}>
-            <h2>{product.name}</h2>
+            <img className="medium" src={product.image} alt={product.name} />
           </a>
-          <div className="card-body">
-            <a href={`/product/${product._id}`}>
-              <img className="medium" src={product.image} alt={product.name} />
-            </a>
-            <h5 className="card-title">{product.gender}</h5>
-            <p className="card-text">{product.description}</p>
-            <a href={`/product/${product._id}`} className="btn btn-primary">
-              Shop Now
-            </a>
-            <Rating
-              rating={product.rating}
-              numReviews={product.numReviews}
-            ></Rating>
-            <div className="price">${product.price}</div>
-          </div>
+          <h5 className="card-title">{product.gender}</h5>
+          <p className="card-text">{product.description}</p>
+          <a href={`/product/${product._id}`} className="btn btn-primary">
+            Shop Now
+          </a>
+          <Rating
+            rating={product.rating}
+            numReviews={product.numReviews}
+          ></Rating>
+          <div className="price">${product.price}</div>
         </div>
-
+      </div>
+    </div>
   );
 }
